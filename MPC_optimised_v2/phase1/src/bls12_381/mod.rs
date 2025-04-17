92a768c44a6ff0ad86b8e86ff1bec21f30a43389
mabingol <mabingol@gmail.com>
2025-04-17 14:25:22 +0100

    17042025

--- MPC_optimised_v2/phase1/src/bls12_381/mod.rs
+++ MPC_optimised_v2/phase1/src/bls12_381/mod.rs
@@ -1,31 +1,3 @@
-//! This ceremony constructs the "powers of tau" for Jens Groth's 2016 zk-SNARK proving
-//! system using the BLS12-381 pairing-friendly elliptic curve construction.
-//!
-//! # Overview
-//!
-//! Participants of the ceremony receive a "challenge" file containing:
-//!
-//! * the BLAKE2b hash of the last file entered into the transcript
-//! * an `Accumulator` (with curve points encoded in uncompressed form for fast deserialization)
-//!
-//! The participant runs a tool which generates a random keypair (`PublicKey`, `PrivateKey`)
-//! used for modifying the `Accumulator` from the "challenge" file. The keypair is then used to
-//! transform the `Accumulator`, and a "response" file is generated containing:
-//!
-//! * the BLAKE2b hash of the "challenge" file (thus forming a hash chain over the entire transcript)
-//! * an `Accumulator` (with curve points encoded in compressed form for fast uploading)
-//! * the `PublicKey`
-//!
-//! This "challenge" file is entered into the protocol transcript. A given transcript is valid
-//! if the transformations between consecutive `Accumulator`s verify with their respective
-//! `PublicKey`s. Participants (and the public) can ensure that their contribution to the
-//! `Accumulator` was accepted by ensuring the transcript contains their "response" file, ideally
-//! by comparison of the BLAKE2b hash of the "response" file.
-//!
-//! After some time has elapsed for participants to contribute to the ceremony, a participant is
-//! simulated with a randomness beacon. The resulting `Accumulator` contains partial zk-SNARK
-//! public parameters for all circuits within a bounded size.
-
 
 extern crate rand;
 extern crate crossbeam;
@@ -36,18 +8,6 @@ extern crate typenum;
 extern crate byteorder;
 
 
-// extern crate bellman;
-// extern crate pairing;
-
-// extern crate pairing_ce;
-// extern crate bellman_ce;
-
-
-// use bellman_ce::pairing::{
-//     CurveAffine, EncodedPoint, Engine, GroupDecodingError, Wnaf
-// };
-// use bellman_ce::pairing::bls12_381::{Bls12, Fr, G1, G2, G1Affine, G2Affine};
-// use crate::parameters::{UseCompression, CheckForCorrectness};
 
 extern crate pairing_ce;
 extern crate bellman_ce;
@@ -68,8 +28,7 @@ use pairing_ce::*;
 use byteorder::{ReadBytesExt, BigEndian};
 use rand::{SeedableRng, Rng, Rand};
 use rand::chacha::ChaChaRng;
-// use bellman::pairing::bls12_381::*;
-// use bellman::pairing::*;
+
 
 
 use std::io::{self, Read, Write};
@@ -79,36 +38,46 @@ use typenum::consts::U64;
 use blake2::{Blake2b, Digest};
 use std::fmt;
 
-
-
-impl Bls12CeremonyParameters {
-    pub const REQUIRED_POWER: usize = 13;
-    pub const TAU_POWERS_G1_LENGTH: usize = 1 << Self::REQUIRED_POWER;
-    pub const TAU_POWERS_LENGTH: usize = (1 << Self::REQUIRED_POWER) + 1;
-
-    // BLS12-381 spesifik değerleri:
-    pub const G1_UNCOMPRESSED_BYTE_SIZE: usize = 96;
-    pub const G2_UNCOMPRESSED_BYTE_SIZE: usize = 192;
-    pub const CONTRIBUTION_BYTE_SIZE: usize = 64; // Güncellenmeli
-    pub const PUBLIC_KEY_SIZE: usize = 96; // Güncellenmeli
-    pub const ACCUMULATOR_BYTE_SIZE: usize = 512; // Güncellenmeli
-}
 use crate::parameters::PowersOfTauParameters;
-
 
 #[derive(Clone)]
 pub struct Bls12CeremonyParameters;
 
 impl PowersOfTauParameters for Bls12CeremonyParameters {
-    const REQUIRED_POWER: usize = 13;
+    const REQUIRED_POWER: usize = 11;
+
     const G1_UNCOMPRESSED_BYTE_SIZE: usize = 96;
     const G2_UNCOMPRESSED_BYTE_SIZE: usize = 192;
     const G1_COMPRESSED_BYTE_SIZE: usize = 48;
     const G2_COMPRESSED_BYTE_SIZE: usize = 96;
 
-       // Eklenmesi gereken sabitler
-       const COMPRESSED_CHALLENGE_LENGTH: usize = 64;
-       const NEW_CHALLENGE_LENGTH: usize = 64;
+    const COMPRESSED_CHALLENGE_LENGTH: usize = 64;
+    const NEW_CHALLENGE_LENGTH: usize = 64;
+
+    // const TAU_POWERS_LENGTH: usize = (1 << Self::REQUIRED_POWER);             // 8192
+    // const TAU_POWERS_G1_LENGTH: usize = (1 << Self::REQUIRED_POWER) * 2 - 1;  // 16383
+
+    // const EMPIRICAL_BATCH_SIZE: usize = 5;  // güvenli ve tutarlı değer
+
+    // const HASH_SIZE: usize = 64;
+
+    // const ACCUMULATOR_BYTE_SIZE: usize =
+    //       Self::G1_UNCOMPRESSED_BYTE_SIZE * Self::TAU_POWERS_G1_LENGTH        
+    //     + Self::G2_UNCOMPRESSED_BYTE_SIZE * Self::TAU_POWERS_LENGTH           
+    //     + Self::G1_UNCOMPRESSED_BYTE_SIZE * Self::TAU_POWERS_LENGTH * 2       
+    //     + Self::G2_UNCOMPRESSED_BYTE_SIZE * 2;                                
+    
+    // const PUBLIC_KEY_SIZE: usize = 3 * Self::G2_UNCOMPRESSED_BYTE_SIZE
+    //                                 + 6 * Self::G1_UNCOMPRESSED_BYTE_SIZE;
+
+    // const CONTRIBUTION_BYTE_SIZE: usize = (Self::TAU_POWERS_G1_LENGTH * Self::G1_COMPRESSED_BYTE_SIZE) 
+    //     + (Self::TAU_POWERS_LENGTH * Self::G2_COMPRESSED_BYTE_SIZE) 
+    //     + (Self::TAU_POWERS_LENGTH * Self::G1_COMPRESSED_BYTE_SIZE)
+    //     + (Self::TAU_POWERS_LENGTH * Self::G1_COMPRESSED_BYTE_SIZE)
+    //     + Self::G2_COMPRESSED_BYTE_SIZE
+    //     + Self::HASH_SIZE
+    //     + Self::PUBLIC_KEY_SIZE;
+
 }
 
 // This ceremony is based on the BLS12-381 elliptic curve construction.
@@ -119,7 +88,7 @@ const G2_COMPRESSED_BYTE_SIZE: usize = 96;
 
 /// The accumulator supports circuits with 2^21 multiplication gates.
 // const TAU_POWERS_LENGTH: usize = (1 << 21);
-const TAU_POWERS_LENGTH: usize = 1 << 13;
+const TAU_POWERS_LENGTH: usize = 1 << 4;
 
 /// More tau powers are needed in G1 because the Groth16 H query
 /// includes terms of the form tau^i * (tau^m - 1) = tau^(i+m) - tau^i
@@ -131,21 +100,21 @@ const TAU_POWERS_G1_LENGTH: usize = (TAU_POWERS_LENGTH << 1) - 1;
 pub const ACCUMULATOR_BYTE_SIZE: usize = (TAU_POWERS_G1_LENGTH * G1_UNCOMPRESSED_BYTE_SIZE) + // g1 tau powers
                                          (TAU_POWERS_LENGTH * G2_UNCOMPRESSED_BYTE_SIZE) + // g2 tau powers
                                          (TAU_POWERS_LENGTH * G1_UNCOMPRESSED_BYTE_SIZE) + // alpha tau powers
-                                         (TAU_POWERS_LENGTH * G1_UNCOMPRESSED_BYTE_SIZE) // beta tau powers
-                                         + G2_UNCOMPRESSED_BYTE_SIZE // beta in g2
+                                         (TAU_POWERS_LENGTH * G1_UNCOMPRESSED_BYTE_SIZE) // Beta tau powers
+                                         + G2_UNCOMPRESSED_BYTE_SIZE // delta in g2
                                          + 64; // blake2b hash of previous contribution
 
 /// The "public key" is used to verify a contribution was correctly
 /// computed.
-pub const PUBLIC_KEY_SIZE: usize = 3 * G2_UNCOMPRESSED_BYTE_SIZE + // tau, alpha, and beta in g2
-                                   6 * G1_UNCOMPRESSED_BYTE_SIZE; // (s1, s1*tau), (s2, s2*alpha), (s3, s3*beta) in g1
+pub const PUBLIC_KEY_SIZE: usize = 3 * G2_UNCOMPRESSED_BYTE_SIZE + // tau, alpha, and delta in g2
+                                   6 * G1_UNCOMPRESSED_BYTE_SIZE; // (s1, s1*tau), (s2, s2*alpha), (s3, s3*delta) in g1
 
-/// The size of the contribution on disk.
+// /// The size of the contribution on disk.
 pub const CONTRIBUTION_BYTE_SIZE: usize = (TAU_POWERS_G1_LENGTH * G1_COMPRESSED_BYTE_SIZE) + // g1 tau powers
                                           (TAU_POWERS_LENGTH * G2_COMPRESSED_BYTE_SIZE) + // g2 tau powers
                                           (TAU_POWERS_LENGTH * G1_COMPRESSED_BYTE_SIZE) + // alpha tau powers
-                                          (TAU_POWERS_LENGTH * G1_COMPRESSED_BYTE_SIZE) // beta tau powers
-                                          + G2_COMPRESSED_BYTE_SIZE // beta in g2
+                                          (TAU_POWERS_LENGTH * G1_COMPRESSED_BYTE_SIZE) // delta tau powers
+                                          + G2_COMPRESSED_BYTE_SIZE // delta in g2
                                           + 64 // blake2b hash of input accumulator
                                           + PUBLIC_KEY_SIZE; // public key
 
@@ -190,17 +159,17 @@ fn test_hash_to_g2() {
 pub struct PublicKey {
     tau_g1: (G1Affine, G1Affine),
     alpha_g1: (G1Affine, G1Affine),
-    beta_g1: (G1Affine, G1Affine),
+    delta_g1: (G1Affine, G1Affine),
     tau_g2: G2Affine,
     alpha_g2: G2Affine,
-    beta_g2: G2Affine
+    delta_g2: G2Affine
 }
 
 /// Contains the secrets τ, α and β that the participant of the ceremony must destroy.
 pub struct PrivateKey {
     tau: Fr,
     alpha: Fr,
-    beta: Fr
+    delta: Fr
 }
 
 /// Constructs a keypair given an RNG and a 64-byte transcript `digest`.
@@ -210,7 +179,7 @@ pub fn keypair<R: Rng>(rng: &mut R, digest: &[u8]) -> (PublicKey, PrivateKey)
 
     let tau = Fr::rand(rng);
     let alpha = Fr::rand(rng);
-    let beta = Fr::rand(rng);
+    let delta = Fr::rand(rng);
 
     let mut op = |x, personalization: u8| {
         // Sample random g^s
@@ -236,21 +205,21 @@ pub fn keypair<R: Rng>(rng: &mut R, digest: &[u8]) -> (PublicKey, PrivateKey)
 
     let pk_tau = op(tau, 0);
     let pk_alpha = op(alpha, 1);
-    let pk_beta = op(beta, 2);
+    let pk_delta = op(delta, 2);
 
     (
         PublicKey {
             tau_g1: pk_tau.0,
             alpha_g1: pk_alpha.0,
-            beta_g1: pk_beta.0,
+            delta_g1: pk_delta.0,
             tau_g2: pk_tau.1,
             alpha_g2: pk_alpha.1,
-            beta_g2: pk_beta.1,
+            delta_g2: pk_delta.1,
         },
         PrivateKey {
             tau: tau,
             alpha: alpha,
-            beta: beta
+            delta: delta
         }
     )
 }
@@ -309,12 +278,12 @@ impl PublicKey {
         write_point(writer, &self.alpha_g1.0, UseCompression::No)?;
         write_point(writer, &self.alpha_g1.1, UseCompression::No)?;
 
-        write_point(writer, &self.beta_g1.0, UseCompression::No)?;
-        write_point(writer, &self.beta_g1.1, UseCompression::No)?;
+        write_point(writer, &self.delta_g1.0, UseCompression::No)?;
+        write_point(writer, &self.delta_g1.1, UseCompression::No)?;
 
         write_point(writer, &self.tau_g2, UseCompression::No)?;
         write_point(writer, &self.alpha_g2, UseCompression::No)?;
-        write_point(writer, &self.beta_g2, UseCompression::No)?;
+        write_point(writer, &self.delta_g2, UseCompression::No)?;
 
         Ok(())
     }
@@ -342,20 +311,20 @@ impl PublicKey {
         let alpha_g1_s = read_uncompressed(reader)?;
         let alpha_g1_s_alpha = read_uncompressed(reader)?;
 
-        let beta_g1_s = read_uncompressed(reader)?;
-        let beta_g1_s_beta = read_uncompressed(reader)?;
+        let delta_g1_s = read_uncompressed(reader)?;
+        let delta_g1_s_delta = read_uncompressed(reader)?;
 
         let tau_g2 = read_uncompressed(reader)?;
         let alpha_g2 = read_uncompressed(reader)?;
-        let beta_g2 = read_uncompressed(reader)?;
+        let delta_g2 = read_uncompressed(reader)?;
 
         Ok(PublicKey {
             tau_g1: (tau_g1_s, tau_g1_s_tau),
             alpha_g1: (alpha_g1_s, alpha_g1_s_alpha),
-            beta_g1: (beta_g1_s, beta_g1_s_beta),
+            delta_g1: (delta_g1_s, delta_g1_s_delta),
             tau_g2: tau_g2,
             alpha_g2: alpha_g2,
-            beta_g2: beta_g2
+            delta_g2: delta_g2
         })
     }
 }
@@ -377,7 +346,7 @@ fn test_pubkey_serialization() {
 /// The `Accumulator` is an object that participants of the ceremony contribute
 /// randomness to. This object contains powers of trapdoor `tau` in G1 and in G2 over
 /// fixed generators, and additionally in G1 over two other generators of exponents
-/// `alpha` and `beta` over those fixed generators. In other words:
+/// `alpha` and `delta` over those fixed generators. In other words:
 ///
 /// * (τ, τ<sup>2</sup>, ..., τ<sup>2<sup>22</sup> - 2</sup>, α, ατ, ατ<sup>2</sup>, ..., ατ<sup>2<sup>21</sup> - 1</sup>, β, βτ, βτ<sup>2</sup>, ..., βτ<sup>2<sup>21</sup> - 1</sup>)<sub>1</sub>
 /// * (β, τ, τ<sup>2</sup>, ..., τ<sup>2<sup>21</sup> - 1</sup>)<sub>2</sub>
@@ -389,10 +358,10 @@ pub struct Accumulator {
     pub tau_powers_g2: Vec<G2Affine>,
     /// alpha * tau^0, alpha * tau^1, alpha * tau^2, ..., alpha * tau^{TAU_POWERS_LENGTH - 1}
     pub alpha_tau_powers_g1: Vec<G1Affine>,
-    /// beta * tau^0, beta * tau^1, beta * tau^2, ..., beta * tau^{TAU_POWERS_LENGTH - 1}
-    pub beta_tau_powers_g1: Vec<G1Affine>,
-    /// beta
-    pub beta_g2: G2Affine
+    /// delta * tau^0, delta * tau^1, delta * tau^2, ..., delta * tau^{TAU_POWERS_LENGTH - 1}
+    pub delta_tau_powers_g1: Vec<G1Affine>,
+    /// delta
+    pub delta_g2: G2Affine
 }
 
 impl Accumulator {
@@ -402,8 +371,8 @@ impl Accumulator {
             tau_powers_g1: vec![G1Affine::one(); TAU_POWERS_G1_LENGTH],
             tau_powers_g2: vec![G2Affine::one(); TAU_POWERS_LENGTH],
             alpha_tau_powers_g1: vec![G1Affine::one(); TAU_POWERS_LENGTH],
-            beta_tau_powers_g1: vec![G1Affine::one(); TAU_POWERS_LENGTH],
-            beta_g2: G2Affine::one()
+            delta_tau_powers_g1: vec![G1Affine::one(); TAU_POWERS_LENGTH],
+            delta_g2: G2Affine::one()
         }
     }
 
@@ -430,8 +399,8 @@ impl Accumulator {
         write_all(writer, &self.tau_powers_g1, compression)?;
         write_all(writer, &self.tau_powers_g2, compression)?;
         write_all(writer, &self.alpha_tau_powers_g1, compression)?;
-        write_all(writer, &self.beta_tau_powers_g1, compression)?;
-        write_all(writer, &[self.beta_g2], compression)?;
+        write_all(writer, &self.delta_tau_powers_g1, compression)?;
+        write_all(writer, &[self.delta_g2], compression)?;
 
         Ok(())
     }
@@ -535,15 +504,15 @@ impl Accumulator {
         let tau_powers_g1 = read_all(reader, TAU_POWERS_G1_LENGTH, compression, checked)?;
         let tau_powers_g2 = read_all(reader, TAU_POWERS_LENGTH, compression, checked)?;
         let alpha_tau_powers_g1 = read_all(reader, TAU_POWERS_LENGTH, compression, checked)?;
-        let beta_tau_powers_g1 = read_all(reader, TAU_POWERS_LENGTH, compression, checked)?;
-        let beta_g2 = read_all(reader, 1, compression, checked)?[0];
+        let delta_tau_powers_g1 = read_all(reader, TAU_POWERS_LENGTH, compression, checked)?;
+        let delta_g2 = read_all(reader, 1, compression, checked)?[0];
 
         Ok(Accumulator {
             tau_powers_g1: tau_powers_g1,
             tau_powers_g2: tau_powers_g2,
             alpha_tau_powers_g1: alpha_tau_powers_g1,
-            beta_tau_powers_g1: beta_tau_powers_g1,
-            beta_g2: beta_g2
+            delta_tau_powers_g1: delta_tau_powers_g1,
+            delta_g2: delta_g2
         })
     }
 
@@ -618,8 +587,8 @@ impl Accumulator {
         batch_exp(&mut self.tau_powers_g1, &taupowers[0..], None);
         batch_exp(&mut self.tau_powers_g2, &taupowers[0..TAU_POWERS_LENGTH], None);
         batch_exp(&mut self.alpha_tau_powers_g1, &taupowers[0..TAU_POWERS_LENGTH], Some(&key.alpha));
-        batch_exp(&mut self.beta_tau_powers_g1, &taupowers[0..TAU_POWERS_LENGTH], Some(&key.beta));
-        self.beta_g2 = self.beta_g2.mul(key.beta).into_affine();
+        batch_exp(&mut self.delta_tau_powers_g1, &taupowers[0..TAU_POWERS_LENGTH], Some(&key.delta));
+        self.delta_g2 = self.delta_g2.mul(key.delta).into_affine();
     }
 }
 
@@ -639,16 +608,16 @@ pub fn verify_transform(before: &Accumulator, after: &Accumulator, key: &PublicK
 
     let tau_g2_s = compute_g2_s(key.tau_g1.0, key.tau_g1.1, 0);
     let alpha_g2_s = compute_g2_s(key.alpha_g1.0, key.alpha_g1.1, 1);
-    let beta_g2_s = compute_g2_s(key.beta_g1.0, key.beta_g1.1, 2);
+    let delta_g2_s = compute_g2_s(key.delta_g1.0, key.delta_g1.1, 2);
 
-    // Check the proofs-of-knowledge for tau/alpha/beta
+    // Check the proofs-of-knowledge for tau/alpha/delta
     if !same_ratio(key.tau_g1, (tau_g2_s, key.tau_g2)) {
         return false;
     }
     if !same_ratio(key.alpha_g1, (alpha_g2_s, key.alpha_g2)) {
         return false;
     }
-    if !same_ratio(key.beta_g1, (beta_g2_s, key.beta_g2)) {
+    if !same_ratio(key.delta_g1, (delta_g2_s, key.delta_g2)) {
         return false;
     }
 
@@ -670,11 +639,11 @@ pub fn verify_transform(before: &Accumulator, after: &Accumulator, key: &PublicK
         return false;
     }
 
-    // Did the participant multiply the previous beta by the new one?
-    if !same_ratio((before.beta_tau_powers_g1[0], after.beta_tau_powers_g1[0]), (beta_g2_s, key.beta_g2)) {
+    // Did the participant multiply the previous delta by the new one?
+    if !same_ratio((before.delta_tau_powers_g1[0], after.delta_tau_powers_g1[0]), (delta_g2_s, key.delta_g2)) {
         return false;
     }
-    if !same_ratio((before.beta_tau_powers_g1[0], after.beta_tau_powers_g1[0]), (before.beta_g2, after.beta_g2)) {
+    if !same_ratio((before.delta_tau_powers_g1[0], after.delta_tau_powers_g1[0]), (before.delta_g2, after.delta_g2)) {
         return false;
     }
 
@@ -688,7 +657,7 @@ pub fn verify_transform(before: &Accumulator, after: &Accumulator, key: &PublicK
     if !same_ratio(power_pairs(&after.alpha_tau_powers_g1), (after.tau_powers_g2[0], after.tau_powers_g2[1])) {
         return false;
     }
-    if !same_ratio(power_pairs(&after.beta_tau_powers_g1), (after.tau_powers_g2[0], after.tau_powers_g2[1])) {
+    if !same_ratio(power_pairs(&after.delta_tau_powers_g1), (after.tau_powers_g2[0], after.tau_powers_g2[1])) {
         return false;
     }
 
