0401c076d61fce81cb53e8ec231f6c2f58f47bc6
mabingol <mabingol@gmail.com>
2025-04-24 12:45:00 +0100

    24042025-3

---
+++ MPC_optimised_v2/phase1/target/release/deps/beacon.d
@@ -0,0 +1,5 @@
+D:\MPC_ceremony\SNARK-MPC-setup\MPC_optimised_v2\phase1\target\release\deps\beacon.exe: src\bin\beacon.rs
+
+D:\MPC_ceremony\SNARK-MPC-setup\MPC_optimised_v2\phase1\target\release\deps\beacon.d: src\bin\beacon.rs
+
+src\bin\beacon.rs:
---
+++ MPC_optimised_v2/phase1/target/release/deps/prepare_phase2.d
@@ -0,0 +1,5 @@
+D:\MPC_ceremony\SNARK-MPC-setup\MPC_optimised_v2\phase1\target\release\deps\prepare_phase2.exe: src\bin\prepare_phase2.rs
+
+D:\MPC_ceremony\SNARK-MPC-setup\MPC_optimised_v2\phase1\target\release\deps\prepare_phase2.d: src\bin\prepare_phase2.rs
+
+src\bin\prepare_phase2.rs:
